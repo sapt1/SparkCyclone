0200af385a3af9c6fc96e8fe0adb2899f196ff4f
Wosin <blind.van@gmail.com>
2021-08-04 14:42:07 +0200

    Some changes to support different inputs, reduce VE usage when not needed.

--- src/main/scala/com/nec/spark/planning/VERewriteStrategy.scala
+++ src/main/scala/com/nec/spark/planning/VERewriteStrategy.scala
@@ -53,8 +53,9 @@ final case class VERewriteStrategy(sparkSession: SparkSession, nativeEvaluator: 
     if (VERewriteStrategy._enabled) {
       plan match {
         case logical.Project(resultExpressions, child) if !resultExpressions.forall {
-              /** If it's just a rename, don't send to VE * */
+              /** If it's just a rename or plain select, don't send to VE * */
               case a: Alias if a.child.isInstanceOf[Attribute] => true
+              case a: AttributeReference                       => true
               case _                                           => false
             } =>
           implicit val nameCleaner: NameCleaner = NameCleaner.verbose
