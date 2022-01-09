479579de7d05ac15368e6d70597e4b8be7514dba
WGipfeli <w@gipfeli.sg>
2022-01-09 08:40:48 +0000

    Try this one la

--- src/test/scala/com/nec/ve/VeKernelInfra.scala
+++ src/test/scala/com/nec/ve/VeKernelInfra.scala
@@ -10,7 +10,7 @@ import java.time.Instant
 trait VeKernelInfra { this: Suite =>
 
   def compiledWithHeaders[T](cCode: CFunction, name: String)(f: Path => T): T = {
-    withCompiled(cCode.toCodeLinesG(name).cCode)(f)
+    withCompiled(cCode.toCodeLinesSPtr(name).cCode)(f)
   }
 
   def compiledWithHeaders[T](cCode: CFunction2, name: String)(f: Path => T): T = {
